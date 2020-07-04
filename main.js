e2ed6f900619f06cb5102e06a00f6b73ea46c447
unknown <jamatheus@gmail.com>
2020-07-04 11:28:31 -0430

    Primer commit de mi sitio web

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-btn').addEventListener('click', function(e) {
+      document.querySelector('.navbar-main ul.nav-brand-menu').classList.toggle('show');
+  });
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.nueva-tarjeta',{delay:500});
+ScrollReveal().reveal('.baner-uno',{delay:500});
+ScrollReveal().reveal('.banner-dos',{delay:500});
